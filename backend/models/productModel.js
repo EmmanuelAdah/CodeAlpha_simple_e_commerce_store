aab44c7fbf5c7939b6b7e9598ea0358f0665bd8a
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-21 20:38:31 +0100

    file:- models

---
+++ backend/models/productModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+
+const productModel = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Product name is required'],
+        minlength: 3,
+        maxlength: 200,
+        unique: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: [true, 'Product price is required'],
+        min: 0,
+        max: 1000000000,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, 'Product description is required'],
+        minlength: 5,
+        maxlength: 10000,
+        trim: true
+    },
+    image: {
+        type: String,
+        required: [true, 'Product image is required'],
+        minlength: 5,
+        maxlength: 10000,
+        trim: true
+    },
+    rating: {
+        type: Number,
+        default: 0,
+        mutable: true,
+        min: 0,
+        max: 5
+    }
+})
+
+module.exports = mongoose.model('Product', productModel);
