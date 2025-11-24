4ff9f1242c8c0fcf5d184c4e774e51d536a62cfb
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-24 01:35:41 +0100

    first commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,16 +1,39 @@
 const Order = require("../models/orderModel");
 const User = require("../models/userModel");
-
+const { getProducts } = require("./productsController");
 
 exports.getOrders = async (req, res) => {
     const { id } = req.params;
+
     try {
         const existingUser = await User.exists({_id: id})
-        if (!existingUser) {
-            return res.status(404).json({error: 'User does not exist'});
-        }
+        if (!existingUser)
+            return res.status(400).json({error: 'User does not exist'});
+
+        const products = await getProducts();
+        if (products.length === 0)
+            return res.status(404).json({message: 'No products found'});
+
         const orders = await Order.find({userId: id});
-        return res.status(200).json(orders);
+        if (orders.length === 0)
+            return res.status(404).json({message: 'No orders found'});
+
+        let usersOrder;
+        let count = 0;
+
+        while(count !== orders.length){
+            let totalAmount = 0;
+            let currentOrderList = orders[count];
+
+            for (let product of products) {
+                if (currentOrderList.some(orderedProduct => orderedProduct._id === product._id)) {
+                    usersOrder[count] += product;
+                    totalAmount += product.price;
+                }
+            }
+            currentOrderList += { 'totalAmount': totalAmount };
+        }
+        return res.status(200).json(usersOrder);
     }catch(err) {
         res.status(500).json({error: 'Something went wrong'});
     }
